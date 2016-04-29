bdd956ef86388b2b5e2dd0a846c2ef3bb56d0674
Emanuele Ruffaldi <emanuele.ruffaldi@gmail.com>
2016-04-29 23:53:58 +0200

    update for gstreamer

--- cmake_modules/FindGStreamer.cmake
+++ cmake_modules/FindGStreamer.cmake
@@ -1,135 +1,174 @@
-FILE(TO_CMAKE_PATH "$ENV{GSTREAMER_DIR}" TRY1_DIR)
-FILE(TO_CMAKE_PATH "${GSTREAMER_DIR}" TRY2_DIR)
-FILE(GLOB GSTREAMER_DIR ${TRY1_DIR} ${TRY2_DIR})
+# - Try to find GStreamer and its plugins
+# Once done, this will define
+#
+#  GSTREAMER_FOUND - system has GStreamer
+#  GSTREAMER_INCLUDE_DIRS - the GStreamer include directories
+#  GSTREAMER_LIBRARIES - link these to use GStreamer
+#
+# Additionally, gstreamer-base is always looked for and required, and
+# the following related variables are defined:
+#
+#  GSTREAMER_BASE_INCLUDE_DIRS - gstreamer-base's include directory
+#  GSTREAMER_BASE_LIBRARIES - link to these to use gstreamer-base
+#
+# Optionally, the COMPONENTS keyword can be passed to find_package()
+# and GStreamer plugins can be looked for.  Currently, the following
+# plugins can be searched, and they define the following variables if
+# found:
+#
+#  gstreamer-app:        GSTREAMER_APP_INCLUDE_DIRS and GSTREAMER_APP_LIBRARIES
+#  gstreamer-audio:      GSTREAMER_AUDIO_INCLUDE_DIRS and GSTREAMER_AUDIO_LIBRARIES
+#  gstreamer-fft:        GSTREAMER_FFT_INCLUDE_DIRS and GSTREAMER_FFT_LIBRARIES
+#  gstreamer-pbutils:    GSTREAMER_PBUTILS_INCLUDE_DIRS and GSTREAMER_PBUTILS_LIBRARIES
+#  gstreamer-video:      GSTREAMER_VIDEO_INCLUDE_DIRS and GSTREAMER_VIDEO_LIBRARIES
+#
+# Copyright (C) 2012 Raphael Kubo da Costa <rakuco@webkit.org>
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1.  Redistributions of source code must retain the above copyright
+#     notice, this list of conditions and the following disclaimer.
+# 2.  Redistributions in binary form must reproduce the above copyright
+#     notice, this list of conditions and the following disclaimer in the
+#     documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER AND ITS CONTRIBUTORS ``AS
+# IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR ITS
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-FIND_PATH(GSTREAMER_gst_INCLUDE_DIR gst/gst.h
-                                    PATHS ${GSTREAMER_INCLUDE_DIR} ${GSTREAMER_DIR}/include /usr/local/include/gstreamer-1.0 /usr/include/gstreamer-1.0
-                                    ENV INCLUDE DOC "Directory containing gst/gst.h include file")
+# Helper macro to find a GStreamer plugin (or GStreamer itself)
+#   _component_prefix is prepended to the _INCLUDE_DIRS and _LIBRARIES variables (eg. "GSTREAMER_AUDIO")
+#   _pkgconfig_name is the component's pkg-config name (eg. "gstreamer-1.0", or "gstreamer-video-1.0").
+#   _header is the component's header, relative to the gstreamer-1.0 directory (eg. "gst/gst.h").
+#   _library is the component's library name (eg. "gstreamer-1.0" or "gstvideo-1.0")
+#macro(FIND_GSTREAMER_COMPONENT _component_prefix _pkgconfig_name _header _library)
+#    pkg_check_modules(PC_${_component_prefix} QUIET ${_pkgconfig_name})
+#
+#    find_path(${_component_prefix}_INCLUDE_DIRS
+#        NAMES ${_header}
+#        HINTS ${PC_${_component_prefix}_INCLUDE_DIRS} ${PC_${_component_prefix}_INCLUDEDIR}
+#        PATH_SUFFIXES gstreamer-1.0
+#    )
+#
+#    find_library(${_component_prefix}_LIBRARIES
+#        NAMES ${_library}
+#        HINTS ${PC_${_component_prefix}_LIBRARY_DIRS} ${PC_${_component_prefix}_LIBDIR}
+#    )
+#endmacro()
 
-FIND_PATH(GSTREAMER_gstconfig_INCLUDE_DIR gst/gstconfig.h
-                                          PATHS ${GSTREAMER_INCLUDE_DIR} ${GSTREAMER_DIR}/include ${GSTREAMER_DIR}/lib/include /usr/local/include/gstreamer-1.0 /usr/include/gstreamer-1.0 /usr/local/lib/include/gstreamer-1.0 /usr/lib/include/gstreamer-1.0
-                                          ENV INCLUDE DOC "Directory containing gst/gstconfig.h include file")
+if (WIN32)
+    macro(FIND_GSTREAMER_COMPONENT _component_prefix _pkgconfig_name _header _library)
+        find_path(${_component_prefix}_INCLUDE_DIRS
+            NAMES ${_header}
+            PATHS C:/gstreamer/1.0/x86_64/include
+            PATH_SUFFIXES gstreamer-1.0
+        )
 
-FIND_LIBRARY(GSTREAMER_gstaudio_LIBRARY NAMES gstaudio-1.0 libgstaudio-1.0
-                                        PATHS ${GSTREAMER_LIB_DIR} ${GSTREAMER_DIR}/bin ${GSTREAMER_DIR}/win32/bin ${GSTREAMER_DIR}/bin/bin C:/gstreamer/bin ${GSTREAMER_DIR}/lib ${GSTREAMER_DIR}/win32/lib /usr/local/lib /usr/lib
-                                        ENV LIB
-                                        DOC "gstaudio library to link with"
-                                        NO_SYSTEM_ENVIRONMENT_PATH)
+        find_library(${_component_prefix}_LIBRARIES
+            NAMES ${_library}
+            PATHS C:/gstreamer/1.0/x86_64/lib
+        )
+    endmacro()
+else ()
 
-FIND_LIBRARY(GSTREAMER_gstapp_LIBRARY NAMES gstapp-1.0 libgstapp-1.0
-                                        PATHS ${GSTREAMER_LIB_DIR} ${GSTREAMER_DIR}/bin ${GSTREAMER_DIR}/win32/bin ${GSTREAMER_DIR}/bin/bin C:/gstreamer/bin ${GSTREAMER_DIR}/lib ${GSTREAMER_DIR}/win32/lib /usr/local/lib /usr/lib
-                                        ENV LIB
-                                        DOC "gstapp library to link with"
-                                        NO_SYSTEM_ENVIRONMENT_PATH)
+    find_package(PkgConfig)    
 
-FIND_LIBRARY(GSTREAMER_gstbase_LIBRARY NAMES gstbase-1.0 libgstbase-1.0
-                                       PATHS ${GSTREAMER_LIB_DIR} ${GSTREAMER_DIR}/bin ${GSTREAMER_DIR}/win32/bin ${GSTREAMER_DIR}/bin/bin C:/gstreamer/bin ${GSTREAMER_DIR}/lib ${GSTREAMER_DIR}/win32/lib /usr/local/lib /usr/lib
-                                       ENV LIB
-                                       DOC "gstbase library to link with"
-                                       NO_SYSTEM_ENVIRONMENT_PATH)
+    macro(FIND_GSTREAMER_COMPONENT _component_prefix _pkgconfig_name _header _library)
+        pkg_check_modules(PC_${_component_prefix} QUIET ${_pkgconfig_name})
 
-FIND_LIBRARY(GLIB_gstcdda_LIBRARY NAMES gstcdda-1.0 libgstcdda-1.0
-                                  PATHS ${GSTREAMER_LIB_DIR} ${GSTREAMER_DIR}/bin ${GSTREAMER_DIR}/win32/bin ${GSTREAMER_DIR}/bin/bin C:/gstreamer/bin ${GSTREAMER_DIR}/lib ${GSTREAMER_DIR}/win32/lib /usr/local/lib /usr/lib
-                                  ENV LIB
-                                  DOC "gstcdda library to link with"
-                                  NO_SYSTEM_ENVIRONMENT_PATH)
+        find_path(${_component_prefix}_INCLUDE_DIRS
+            NAMES ${_header}
+            HINTS ${PC_${_component_prefix}_INCLUDE_DIRS} ${PC_${_component_prefix}_INCLUDEDIR}
+            PATH_SUFFIXES gstreamer-1.0
+        )
 
-FIND_LIBRARY(GSTREAMER_gstcontroller_LIBRARY NAMES gstcontroller-1.0 libgstcontroller-1.0
-                                             PATHS ${GSTREAMER_LIB_DIR} ${GSTREAMER_DIR}/bin ${GSTREAMER_DIR}/win32/bin ${GSTREAMER_DIR}/bin/bin C:/gstreamer/bin ${GSTREAMER_DIR}/lib ${GSTREAMER_DIR}/win32/lib /usr/local/lib /usr/lib
-                                             ENV LIB
-                                             DOC "gstcontroller library to link with"
-                                             NO_SYSTEM_ENVIRONMENT_PATH)
-if(0)
-FIND_LIBRARY(GSTREAMER_gstdataprotocol_LIBRARY NAMES gstdataprotocol-1.0 libgstdataprotocol-1.0
-                                               PATHS ${GSTREAMER_LIB_DIR} ${GSTREAMER_DIR}/bin ${GSTREAMER_DIR}/win32/bin ${GSTREAMER_DIR}/bin/bin C:/gstreamer/bin ${GSTREAMER_DIR}/lib ${GSTREAMER_DIR}/win32/lib /usr/local/lib /usr/lib
-                                               ENV LIB
-                                               DOC "gstdataprotocol library to link with"
-                                               NO_SYSTEM_ENVIRONMENT_PATH)
+        find_library(${_component_prefix}_LIBRARIES
+            NAMES ${_library}
+            HINTS ${PC_${_component_prefix}_LIBRARY_DIRS} ${PC_${_component_prefix}_LIBDIR}
+        )
+    endmacro()
+endif ()
 
-FIND_LIBRARY(GSTREAMER_gstinterfaces_LIBRARY NAMES gstinterfaces-1.0 libgstinterfaces-1.0
-                                             PATHS ${GSTREAMER_LIB_DIR} ${GSTREAMER_DIR}/bin ${GSTREAMER_DIR}/win32/bin ${GSTREAMER_DIR}/bin/bin C:/gstreamer/bin ${GSTREAMER_DIR}/lib ${GSTREAMER_DIR}/win32/lib /usr/local/lib /usr/lib
-                                             ENV LIB
-                                             DOC "gstinterfaces library to link with"
-                                             NO_SYSTEM_ENVIRONMENT_PATH)
+# ------------------------
+# 1. Find GStreamer itself
+# ------------------------
 
-FIND_LIBRARY(GSTREAMER_gstnet_LIBRARY NAMES gstnet-1.0 libgstnet-1.0
-                                      PATHS ${GSTREAMER_LIB_DIR} ${GSTREAMER_DIR}/bin ${GSTREAMER_DIR}/win32/bin ${GSTREAMER_DIR}/bin/bin C:/gstreamer/bin ${GSTREAMER_DIR}/lib ${GSTREAMER_DIR}/win32/lib /usr/local/lib /usr/lib
-                                      ENV LIB
-                                      DOC "gstnet library to link with"
-                                      NO_SYSTEM_ENVIRONMENT_PATH)
+# 1.1. Find headers and libraries
+FIND_GSTREAMER_COMPONENT(GSTREAMER gstreamer-1.0 gst/gst.h gstreamer-1.0)
+FIND_GSTREAMER_COMPONENT(GSTREAMER_BASE gstreamer-base-1.0 gst/gst.h gstbase-1.0)
 
-FIND_LIBRARY(GSTREAMER_gstnetbuffer_LIBRARY NAMES gstnetbuffer-1.0 libgstnetbuffer-1.0
-                                            PATHS ${GSTREAMER_DIR}/bin ${GSTREAMER_DIR}/win32/bin ${GSTREAMER_DIR}/bin/bin C:/gstreamer/bin ${GSTREAMER_DIR}/lib ${GSTREAMER_DIR}/win32/lib /usr/local/lib /usr/lib
-                                            ENV LIB
-                                            DOC "gstnetbuffer library to link with"
-                                            NO_SYSTEM_ENVIRONMENT_PATH)
 
-FIND_LIBRARY(GSTREAMER_gstpbutils_LIBRARY NAMES gstpbutils-1.0 libgstpbutils-1.0
-                                          PATHS ${GSTREAMER_LIB_DIR} ${GSTREAMER_DIR}/bin ${GSTREAMER_DIR}/win32/bin ${GSTREAMER_DIR}/bin/bin C:/gstreamer/bin ${GSTREAMER_DIR}/lib ${GSTREAMER_DIR}/win32/lib /usr/local/lib /usr/lib
-                                          ENV LIB
-                                          DOC "gstpbutils library to link with"
-                                          NO_SYSTEM_ENVIRONMENT_PATH)
+# 1.2. Check GStreamer version
+if (GSTREAMER_INCLUDE_DIRS)
+    if (EXISTS "${GSTREAMER_INCLUDE_DIRS}/gst/gstversion.h")
+        file(READ "${GSTREAMER_INCLUDE_DIRS}/gst/gstversion.h" GSTREAMER_VERSION_CONTENTS)
 
-FIND_LIBRARY(GSTREAMER_gstreamer_LIBRARY NAMES gstreamer-1.0 libgstreamer-1.0 gstreamer-1.0.0
-                                         PATHS ${GSTREAMER_LIB_DIR} ${GSTREAMER_DIR}/bin ${GSTREAMER_DIR}/win32/bin ${GSTREAMER_DIR}/bin/bin C:/gstreamer/bin ${GSTREAMER_DIR}/lib ${GSTREAMER_DIR}/win32/lib /usr/local/lib /usr/lib
-                                         ENV LIB
-                                         DOC "gstreamer library to link with"
-                                         NO_SYSTEM_ENVIRONMENT_PATH)
+        string(REGEX MATCH "#define +GST_VERSION_MAJOR +\\(([0-9]+)\\)" _dummy "${GSTREAMER_VERSION_CONTENTS}")
+        set(GSTREAMER_VERSION_MAJOR "${CMAKE_MATCH_1}")
 
-FIND_LIBRARY(GSTREAMER_gstriff_LIBRARY NAMES gstriff-1.0 libgstriff-1.0
-                                             PATHS ${GSTREAMER_LIB_DIR} ${GSTREAMER_DIR}/bin ${GSTREAMER_DIR}/win32/bin ${GSTREAMER_DIR}/bin/bin C:/gstreamer/bin ${GSTREAMER_DIR}/lib ${GSTREAMER_DIR}/win32/lib /usr/local/lib /usr/lib
-                                             ENV LIB
-                                             DOC "gstriff library to link with"
-                                             NO_SYSTEM_ENVIRONMENT_PATH)
+        string(REGEX MATCH "#define +GST_VERSION_MINOR +\\(([0-9]+)\\)" _dummy "${GSTREAMER_VERSION_CONTENTS}")
+        set(GSTREAMER_VERSION_MINOR "${CMAKE_MATCH_1}")
 
-FIND_LIBRARY(GSTREAMER_gstrtp_LIBRARY NAMES gstrtp-1.0 libgstrtp-1.0
-                                    PATHS ${GSTREAMER_LIB_DIR} ${GSTREAMER_DIR}/bin ${GSTREAMER_DIR}/win32/bin ${GSTREAMER_DIR}/bin/bin C:/gstreamer/bin ${GSTREAMER_DIR}/lib ${GSTREAMER_DIR}/win32/lib /usr/local/lib /usr/lib
-                                    ENV LIB
-                                    DOC "gstrtp library to link with"
-                                    NO_SYSTEM_ENVIRONMENT_PATH)
+        string(REGEX MATCH "#define +GST_VERSION_MICRO +\\(([0-9]+)\\)" _dummy "${GSTREAMER_VERSION_CONTENTS}")
+        set(GSTREAMER_VERSION_MICRO "${CMAKE_MATCH_1}")
 
-FIND_LIBRARY(GSTREAMER_gstrtsp_LIBRARY NAMES gstrtsp-1.0 libgstrtsp-1.0
-                                       PATHS ${GSTREAMER_LIB_DIR} ${GSTREAMER_DIR}/bin ${GSTREAMER_DIR}/win32/bin ${GSTREAMER_DIR}/bin/bin C:/gstreamer/bin ${GSTREAMER_DIR}/lib ${GSTREAMER_DIR}/win32/lib /usr/local/lib /usr/lib
-                                       ENV LIB
-                                       DOC "gstrtsp library to link with"
-                                       NO_SYSTEM_ENVIRONMENT_PATH)
+        set(GSTREAMER_VERSION "${GSTREAMER_VERSION_MAJOR}.${GSTREAMER_VERSION_MINOR}.${GSTREAMER_VERSION_MICRO}")
+    endif ()
+endif ()
 
-FIND_LIBRARY(GSTREAMER_gstsdp_LIBRARY NAMES gstsdp-1.0 libgstsdp-1.0
-                                      PATHS ${GSTREAMER_LIB_DIR} ${GSTREAMER_DIR}/bin ${GSTREAMER_DIR}/win32/bin ${GSTREAMER_DIR}/bin/bin C:/gstreamer/bin ${GSTREAMER_DIR}/lib ${GSTREAMER_DIR}/win32/lib /usr/local/lib /usr/lib
-                                      ENV LIB
-                                      DOC "gstsdp library to link with"
-                                      NO_SYSTEM_ENVIRONMENT_PATH)
 
-FIND_LIBRARY(GSTREAMER_gsttag_LIBRARY NAMES gsttag-1.0 libgsttag-1.0
-                                 PATHS ${GSTREAMER_LIB_DIR} ${GSTREAMER_DIR}/bin ${GSTREAMER_DIR}/win32/bin ${GSTREAMER_DIR}/bin/bin C:/gstreamer/bin ${GSTREAMER_DIR}/lib ${GSTREAMER_DIR}/win32/lib /usr/local/lib /usr/lib
-                                 ENV LIB
-                                 DOC "gsttag library to link with"
-                                 NO_SYSTEM_ENVIRONMENT_PATH)
+if ("${GStreamer_FIND_VERSION}" VERSION_GREATER "${GSTREAMER_VERSION}")
+    message(FATAL_ERROR "Required version (" ${GStreamer_FIND_VERSION} ") is higher than found version (" ${GSTREAMER_VERSION} ")")
+endif ()
 
-FIND_LIBRARY(GSTREAMER_gstvideo_LIBRARY NAMES gstvideo-1.0 libgstvideo-1.0
-                                        PATHS ${GSTREAMER_LIB_DIR} ${GSTREAMER_DIR}/bin ${GSTREAMER_DIR}/win32/bin ${GSTREAMER_DIR}/bin/bin C:/gstreamer/bin ${GSTREAMER_DIR}/lib ${GSTREAMER_DIR}/win32/lib /usr/local/lib /usr/lib
-                                        ENV LIB
-                                        DOC "gstvideo library to link with"
-                                        NO_SYSTEM_ENVIRONMENT_PATH)
-endif()
-#AND GSTREAMER_gstconfig_INCLUDE_DIR AND
-   # GSTREAMER_gstaudio_LIBRARY AND GSTREAMER_gstbase_LIBRARY AND GSTREAMER_gstcontroller_LIBRARY AND
-   # GSTREAMER_gstdataprotocol_LIBRARY AND GSTREAMER_gstinterfaces_LIBRARY AND GSTREAMER_gstnet_LIBRARY AND
-   # GSTREAMER_gstnetbuffer_LIBRARY AND GSTREAMER_gstpbutils_LIBRARY AND GSTREAMER_gstreamer_LIBRARY AND
-   # GSTREAMER_gstriff_LIBRARY AND GSTREAMER_gstrtp_LIBRARY AND GSTREAMER_gstrtsp_LIBRARY AND GSTREAMER_gstsdp_LIBRARY AND
-    #GSTREAMER_gsttag_LIBRARY AND GSTREAMER_gstvideo_LIBRARY
+# -------------------------
+# 2. Find GStreamer plugins
+# -------------------------
 
-IF (GSTREAMER_gst_INCLUDE_DIR )
-  SET(GSTREAMER_INCLUDE_DIR ${GSTREAMER_gst_INCLUDE_DIR} ${GSTREAMER_gstconfig_INCLUDE_DIR})
-  list(REMOVE_DUPLICATES GSTREAMER_INCLUDE_DIR)
-  SET(GSTREAMER_LIBRARIES ${GSTREAMER_gstaudio_LIBRARY} ${GSTREAMER_gstbase_LIBRARY}
-                          ${GSTREAMER_gstcontroller_LIBRARY} ${GSTREAMER_gstdataprotocol_LIBRARY} ${GSTREAMER_gstinterfaces_LIBRARY}
-                          ${GSTREAMER_gstapp_LIBRARY} ${GSTREAMER_gstnet_LIBRARY} ${GSTREAMER_gstnetbuffer_LIBRARY} ${GSTREAMER_gstpbutils_LIBRARY}
-                          ${GSTREAMER_gstreamer_LIBRARY} ${GSTREAMER_gstriff_LIBRARY} ${GSTREAMER_gstrtp_LIBRARY}
-                          ${GSTREAMER_gstrtsp_LIBRARY} ${GSTREAMER_gstsdp_LIBRARY} ${GSTREAMER_gsttag_LIBRARY} ${GSTREAMER_gstvideo_LIBRARY})
-  list(REMOVE_DUPLICATES GSTREAMER_LIBRARIES)
-  message(gstreamer ${GSTREAMER_LIBRARIES})
-  SET(GSTREAMER_FOUND TRUE)
-else()
-message(gstreamer missing! ${GSTREAMER_LIB_DIR})
-ENDIF()
+FIND_GSTREAMER_COMPONENT(GSTREAMER_APP gstreamer-app-1.0 gst/app/gstappsink.h gstapp-1.0)
+FIND_GSTREAMER_COMPONENT(GSTREAMER_AUDIO gstreamer-audio-1.0 gst/audio/audio.h gstaudio-1.0)
+FIND_GSTREAMER_COMPONENT(GSTREAMER_FFT gstreamer-fft-1.0 gst/fft/gstfft.h gstfft-1.0)
+FIND_GSTREAMER_COMPONENT(GSTREAMER_PBUTILS gstreamer-pbutils-1.0 gst/pbutils/pbutils.h gstpbutils-1.0)
+FIND_GSTREAMER_COMPONENT(GSTREAMER_VIDEO gstreamer-video-1.0 gst/video/video.h gstvideo-1.0)
+
+# ------------------------------------------------
+# 3. Process the COMPONENTS passed to FIND_PACKAGE
+# ------------------------------------------------
+set(_GSTREAMER_REQUIRED_VARS GSTREAMER_INCLUDE_DIRS GSTREAMER_LIBRARIES GSTREAMER_VERSION GSTREAMER_BASE_INCLUDE_DIRS GSTREAMER_BASE_LIBRARIES)
+
+foreach (_component ${GStreamer_FIND_COMPONENTS})
+    set(_gst_component "GSTREAMER_${_component}")
+    string(TOUPPER ${_gst_component} _UPPER_NAME)
+
+    list(APPEND _GSTREAMER_REQUIRED_VARS ${_UPPER_NAME}_INCLUDE_DIRS ${_UPPER_NAME}_LIBRARIES)
+endforeach ()
+
+include(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(GStreamer REQUIRED_VARS ${_GSTREAMER_REQUIRED_VARS}
+                                            VERSION_VAR   GSTREAMER_VERSION)
+
+mark_as_advanced(
+    GSTREAMER_APP_INCLUDE_DIRS
+    GSTREAMER_APP_LIBRARIES
+    GSTREAMER_AUDIO_INCLUDE_DIRS
+    GSTREAMER_AUDIO_LIBRARIES
+    GSTREAMER_BASE_INCLUDE_DIRS
+    GSTREAMER_BASE_LIBRARIES
+    GSTREAMER_FFT_INCLUDE_DIRS
+    GSTREAMER_FFT_LIBRARIES
+    GSTREAMER_INCLUDE_DIRS
+    GSTREAMER_LIBRARIES
+    GSTREAMER_PBUTILS_INCLUDE_DIRS
+    GSTREAMER_PBUTILS_LIBRARIES
+    GSTREAMER_VIDEO_INCLUDE_DIRS
+    GSTREAMER_VIDEO_LIBRARIES
+)
